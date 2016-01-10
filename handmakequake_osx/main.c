d1e36f5f4fc9dec4591ee457bdcf7432715f6588
eggbit <gitrod@gmail.com>
2016-01-10 15:11:55 -0500

    Gave a better return type to CheckParam and implemented a frame rate cap to avoid high CPU usage during testing.

--- handmakequake_osx/main.c
+++ handmakequake_osx/main.c
@@ -5,7 +5,7 @@
 #include <sdl.h>
 
 // Check if the command line argument 'search_arg' exists.  If it does, return the associated value.
-// If it doesn't exist, return "-1" so atoi (et al) won't choke.
+// If it doesn't exist, return null terminator so atoi (et al) won't choke.
 //
 //      eg. quake -setalpha 50 -startwindowed
 //
@@ -30,7 +30,7 @@ CheckParam(const char *search_arg, uint32_t num_args, const char *args[]) {
         }
     }
     // Parameter not found
-    return "-1";
+    return "\0";
 }
 
 int
@@ -45,8 +45,8 @@ main(int argc, const char *argv[]) {
     int32_t width = atoi(CheckParam("-width", argc, argv));
     int32_t height = atoi(CheckParam("-height", argc, argv));
     
-    if(width < 0) width = 640;
-    if(height < 0) height = 480;
+    if(!width) width = 640;
+    if(!height) height = 480;
     
     window = SDL_CreateWindow("handmadequake_osx", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
     if(!window) goto error;
@@ -59,11 +59,12 @@ main(int argc, const char *argv[]) {
     
     // Main loop
     for(;;) {
+        // For use in capping the framerate below.
+        int32_t ticks_start = SDL_GetTicks();
+        
         // Event processing
         while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT) {
-                goto exit;
-            }
+            if(event.type == SDL_QUIT) goto exit;
             
             if(event.type == SDL_MOUSEBUTTONDOWN) {
                 r = rand() % 255;
@@ -77,6 +78,14 @@ main(int argc, const char *argv[]) {
         SDL_SetRenderDrawColor(renderer, r, g, b, a);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
+        
+        // Cap the framerate to avoid high CPU usage from SDL_PollEvent.
+        //  1. Calculate our frame time: (1000 / frames_per_second_we_want)
+        //  2. Subtract that from the current ticks subtracted by the ticks we got at the start of the frame.
+        int32_t sleep_time = (1000 / 60) - (SDL_GetTicks() - ticks_start);
+        
+        // If sleep_time is greater than zero, sleep for that amount of time.
+        if(sleep_time > 0) SDL_Delay(sleep_time);
     }
     
 error:
