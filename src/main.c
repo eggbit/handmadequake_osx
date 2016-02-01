eec427ca5ed84593d7a17d7fb8bcab960f204e6a
eggbit <gitrod@gmail.com>
2016-02-01 16:43:13 -0500

    sys_init_float_time isn't necessary anymore and neither is the lone global.

--- src/main.c
+++ src/main.c
@@ -3,21 +3,13 @@
 
 // IDEA: Timer structure?
 
-static u64 g_time_count = 0;
-
 double
-sys_init_float_time(void) {
-    g_time_count = SDL_GetPerformanceCounter();
-    return 0;
-}
-
-double
-sys_float_time(double seconds_per_tick) {
+sys_float_time(u64 *time_counter, double seconds_per_tick) {
     static double time_passed = 0;
     u64 counter = SDL_GetPerformanceCounter();
 
-    u64 interval = counter - g_time_count;
-    g_time_count = counter;
+    u64 interval = counter - *time_counter;
+    *time_counter = counter;
 
     return time_passed += (double)interval * seconds_per_tick;
 }
@@ -47,13 +39,14 @@ main(int argc, const char *argv[]) {
 
     if(!sdl_init("Handmade Quake OSX", width, height, &window, &renderer)) goto error;
 
-    double seconds_per_tick = 1.0 / (double)SDL_GetPerformanceFrequency();
-    double newtime = 0.0, time_accumulated = 0.0;
-    double target_fps = 1.0 / 30.0;
-    double oldtime = sys_init_float_time();
-
-    SDL_Color rgb = {0, 0, 0, 255};
     host_init();
+    SDL_Color rgb = {0, 0, 0, 255};
+
+    // Set up timers and frame rate
+    double seconds_per_tick = 1.0 / (double)SDL_GetPerformanceFrequency();
+    double newtime = 0.0, oldtime = 0.0, time_accumulated = 0.0;
+    double target_fps = 1.0 / 60.0;
+    u64 time_count = SDL_GetPerformanceCounter();
 
     // Main loop
     for(;;) {
@@ -68,12 +61,14 @@ main(int argc, const char *argv[]) {
 
         sdl_flush_events();
 
-        newtime = sys_float_time(seconds_per_tick);
+        // Update timers
+        newtime = sys_float_time(&time_count, seconds_per_tick);
         time_accumulated += newtime - oldtime;
         oldtime = newtime;
 
         if (time_accumulated > target_fps)
         {
+            // Rendering
             host_frame(target_fps);
 
             SDL_SetRenderDrawColor(renderer, rgb.r, rgb.g, rgb.b, rgb.a);
